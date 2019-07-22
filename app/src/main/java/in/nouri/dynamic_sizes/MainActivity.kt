fd505a4e4ccc7a81c32d35c321f823b5a984106e
Mohammad <nouri.softeng@gmail.com>
2019-07-22 17:05:33 +0430

    First commit

---
+++ app/src/main/java/in/nouri/dynamic_sizes/MainActivity.kt
@@ -0,0 +1,12 @@
+package `in`.nouri.dynamic_sizes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
